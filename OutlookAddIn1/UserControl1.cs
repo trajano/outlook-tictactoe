617db47de2dcfc2ab1478e9c90477a2d7570c313
Archimedes Trajano <arch@trajano.net>
2016-11-23 23:52:24 -0500

    Added a callback from HTMLPage1
    This is used to trigger something on the app.

--- OutlookAddIn1/UserControl1.cs
+++ OutlookAddIn1/UserControl1.cs
@@ -12,15 +12,34 @@ namespace OutlookAddIn1
 {
     public partial class UserControl1 : UserControl
     {
+        private readonly Uri initialUri;
         public UserControl1()
         {
             InitializeComponent();
             System.Reflection.Assembly assemblyInfo = System.Reflection.Assembly.GetExecutingAssembly();
             Uri uriCodeBase = new Uri(assemblyInfo.CodeBase);
 
-            this.webBrowser2.Url = new Uri(uriCodeBase, @"HTMLPage1.html");
+            this.webBrowser2.Navigating += WebBrowser2_Navigating;
+            initialUri = new Uri(uriCodeBase, @"HTMLPage1.html");
+            this.webBrowser2.Url = initialUri;
 
         }
 
+        /// <summary>
+        /// Checks if the URL is navigating to a page I am expecting namely the close event handler otherwise just silently ignore.  It will only
+        /// allow nativation to the initial uri.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebBrowser2_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            e.Cancel = initialUri != e.Url;
+            if (e.Url.Scheme == "myapp") {
+                if (e.Url.PathAndQuery == "/closeme")
+                {
+                    Globals.ThisAddIn.toggleCustomTaskPane();
+                }
+            }
+        }
     }
 }
